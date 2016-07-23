3e42ae3c9005c449342932e00b2220ce5613d003
taojt <taojt08@163.com>
2016-07-23 12:50:36 +0800

    动态替换fragment

--- app/src/main/java/com/example/taojt/fragmenttest/MainActivity.java
+++ app/src/main/java/com/example/taojt/fragmenttest/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.taojt.fragmenttest;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AnotherRightFragment anotherRightFragment = new AnotherRightFragment();
+                android.app.FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.right_layout, anotherRightFragment);
+                transaction.commit();
+
+            }
+        });
     }
 }
