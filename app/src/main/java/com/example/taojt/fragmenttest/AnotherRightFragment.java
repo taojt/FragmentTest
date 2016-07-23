69ae61c7fb7a34a545528888477492692044803f
taojt <taojt08@163.com>
2016-07-23 12:35:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/taojt/fragmenttest/AnotherRightFragment.java
@@ -0,0 +1,20 @@
+package com.example.taojt.fragmenttest;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by taojt on 2016/7/23.
+ */
+public class AnotherRightFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.another_right_fragment, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/taojt/fragmenttest/RightFragment.java
@@ -0,0 +1,20 @@
+package com.example.taojt.fragmenttest;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by taojt on 2016/7/23.
+ */
+public class RightFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.right_frament,container, false);
+        return view;
+    }
+}
